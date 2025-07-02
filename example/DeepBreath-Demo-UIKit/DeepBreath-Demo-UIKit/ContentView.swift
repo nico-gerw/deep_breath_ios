a25269e3d3461e7ef739f5311757f32f8d7e23c0
Nico Gerwien <n.gerwien@gmail.com>
2025-07-02 14:46:42 +0200

    initial commit

---
+++ example/DeepBreath-Demo-UIKit/DeepBreath-Demo-UIKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepBreath-Demo-UIKit
+//
+//  Created by Nico Gerwien on 02.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ example/DeepBreath-Demo-UIKit/DeepBreath-Demo-UIKit/DeepBreath_Demo_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepBreath_Demo_UIKitApp.swift
+//  DeepBreath-Demo-UIKit
+//
+//  Created by Nico Gerwien on 02.07.25.
+//
+
+import SwiftUI
+
+@main
+struct DeepBreath_Demo_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
